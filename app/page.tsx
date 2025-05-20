a0c448f7d640257ad53d1df338b39f13fbcf0ec9
Afolabi Marcus <afolabi.marcus@pawait.co.ke>
2025-05-20 16:03:12 +0300

    Removed unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 import React from "react";
-import TitleBar from "./components/common/TitleBar";
-// import TotalSalesAreaChart from "./components/TotalSalesAreaChart";
-// import TotalRevenueAreaChart from "./components/TotalRevenueAreaChart";
-// import TotalProfitAreaChart from "./components/TotalProfitAreaChart";
-// import RecentSales from "./components/RecentSales";
-// import Button from "./components/ui/Button";
-// import { FiExternalLink } from "react-icons/fi";
-// import RecentProducts from "./components/RecentProducts";
-// import RecentCustomers from "./components/RecentCustomers";
+import TitleBar from "./components/common/TitleBar"; 
 
 export default function Home() {
   return (
