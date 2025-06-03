5b7e70b7c8f6a2943231da88430d76bbc572c678
Afolabi Marcus <afolabi.marcus@pawait.co.ke>
2025-06-03 10:33:59 +0300

    added the looker insight to the insight dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 
 import React from "react";
-import TitleBar from "./components/common/TitleBar"; 
 
 export default function Home() {
   return (
-    <>
-      <div className="space-y-4">
-        <TitleBar
-          title="Insights"
-          buttonLabel="Export Reports"
-          buttonLink="/api/reports/export"
-          showDateRange={true}
-        />
-      </div>
-    </>
+    <div className="space-y-4">
+      <iframe
+        width="100%"
+        height="900vh"
+        src="https://lookerstudio.google.com/embed/reporting/1d44cb9e-656d-4299-b53d-7e1e951db74b/page/p_uf4yrqljsd"
+        style={{ border: 0 }}
+        allowFullScreen
+        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+      ></iframe>
+    </div>
   );
 }
